9aa6042fd9418f9660648e6883d2e447dbd06978
Yair Silbermintz <Yair@Silbermintz.com>
2013-03-12 12:44:40 -0400

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+Pull, Clone, or unzip to your project directory and get coding with jquery and twitter boostrap ready to go.
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Basic Website Template 2013</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="css/main.css" rel="stylesheet" media="screen">
+	</head>
+	<body>
+		<h1>Hello, world!</h1>
+
+
+		<!-- javascript -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+		<script src="js/main.js"></script>
+	</body>
+</html>
